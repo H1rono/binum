c54d5218fa518278a65512f8d91d0d73dade9b0d
H1rono <hronok66@gmail.com>
2022-11-21 21:29:44 +0900

    :sparkles: Implement Euclidean Algorithm in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,46 @@
-// demonstration: ABC210 A - Cabbages
-//  https://atcoder.jp/contests/abc210/tasks/abc210_a
+// demonstration: Euclidean Algorithm
+use std::{
+    cmp,
+    io::{self, Write},
+};
 
 use binum::UInt;
 
+fn gcd(a: UInt, b: UInt) -> UInt {
+    let mut ma = cmp::max(a.clone(), b.clone());
+    let mut mi = cmp::min(a, b);
+    while mi > UInt::from(0) {
+        (ma, mi) = (mi.clone(), ma % mi);
+    }
+    ma
+}
+
 fn main() {
-    let stdin = std::io::stdin();
-    let line = stdin.lines().next().unwrap().unwrap();
-    let (n, a, x, y) = {
-        let v = line
-            .split_whitespace()
-            .map(|x| x.parse::<u64>().unwrap())
-            .collect::<Vec<_>>();
-        let f = |i| UInt::from(v[i]);
-        (f(0), f(1), f(2), f(3))
-    };
-    let ans = if n <= a {
-        x * n
-    } else {
-        x * a.clone() + (n - a) * y
-    };
-    println!("{}", u64::from(ans));
+    let mut lines = io::stdin().lines();
+    let mut stdout = io::stdout();
+    stdout
+        .write_all(b"calculate a % b\n")
+        .expect("failed to write message");
+    stdout.write_all(b"a: ").expect("failed to write message");
+    stdout.flush().expect("failed to flush stdout");
+    let a = lines
+        .next()
+        .expect("too few input lines")
+        .expect("too few input lines")
+        .parse::<u64>()
+        .expect("please input number");
+    let a = UInt::from(a);
+    stdout.write_all(b"b: ").expect("failed to write message");
+    stdout.flush().expect("failed to flush stdout");
+    let b = lines
+        .next()
+        .expect("too few input lines")
+        .expect("too few input lines")
+        .parse::<u64>()
+        .expect("please input number");
+    let b = UInt::from(b);
+    let c = gcd(a, b);
+    stdout
+        .write_all(format!("gcd(a, b) is {}", u64::from(c)).as_bytes())
+        .expect("failed to write message");
 }
